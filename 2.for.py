c94db862b0564f6eedbf824f9af177c6e75d2b27
DK-94 <david.kiberone@gmail.com>
2022-01-07 16:55:22 +0400

    for example

--- 2.for.py
+++ 2.for.py
@@ -5,3 +5,11 @@ print(my_l)
 for adada in range(4,10,1):
     my_l.append(adada)
 print(my_l)
+
+l_2 = ["ad","da", "ab", "ba"]
+
+my_l.extend(l_2)
+print(my_l)
+z = len(my_l)
+for az in range(0,z,2):
+    print(my_l[az])
---
+++ main.py
@@ -0,0 +1,10 @@
+a = int(input("a = "))
+b = int(input("b = "))
+
+# TODO միջին թվաբանական
+c = (a+b)/2
+
+print("c = ", c)
+print(type(c))
+
+
